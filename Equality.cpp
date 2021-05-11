aa32a625202265b3da4d4af2ea39bea523b6345a
TRECN <78320320+TRECN@users.noreply.github.com>
2021-05-11 19:41:55 +0530

    commit

--- Equality.cpp
+++ Equality.cpp
@@ -8,10 +8,12 @@ int main(){
         cin>>n;
         
 
+        
+
         int a[n];
         for(int i=0;i<n;i++){
 
-            
+
             cin>>a[i];
         }
         for(int i=0;i<n;i++){
