d77c42fb4062ea0d94e01c10a485265df3b1f7f6
TRECN <78320320+TRECN@users.noreply.github.com>
2021-05-11 19:10:22 +0530

    first cummit

---
+++ Equality.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int t,n,count=0;
+    cin>>t;
+    while(t--){
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        for(int i=0;i<n;i++){
+            if(a[i]==i){
+                count++;
+            }
+        }
+        cout<<count<<endl;
+        count=0;
+    }
+    return 0;
+
+}
