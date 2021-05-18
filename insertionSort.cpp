ef4fc49f75139281e6df6f43eca165ec94b7b8b9
TRECN <78320320+TRECN@users.noreply.github.com>
2021-05-18 08:21:00 +0530

    commit Insertion Sort

---
+++ insertionSort.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+     for(int i=0;i<n;i++){
+         int c=a[i];
+         int j=i-1;
+         while(a[j]>c&&j>=0){
+             a[i]=a[j];
+             j--;
+         }
+         a[j+1]=c;
+     }
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+
+}
