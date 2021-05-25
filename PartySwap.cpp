c6de8042a24211c076df075b236ad54cd7aaf4ce
TRECN <rishabh1103@outlook.com>
2021-05-25 21:30:41 +0530

    commit

--- PartySwap.cpp
+++ PartySwap.cpp
@@ -4,27 +4,22 @@ using namespace std;
 int main(){
     int t,n;
     cin>>t;
-    
     while(t--){
         cin>>n;
         int a[n];
         for(int k=0;k<n;k++){
             cin>>a[k];
         }
-        int i=0;
-        while(i<n-1){
+        for(int i=1;i<n;i++){
             int j=abs(a[i]%2);
-
-            int l=abs(a[i+1]%2);
+            int l=abs(a[i-1]%2);
             if(j!=l){
                 int t=a[i];
                 a[i]=a[i+1];
                 a[i+1]=t;
                 
             }
-            i++;
         }
-
         for(int k=0;k<n;k++){
             cout<<a[k]<<" ";
         }
