a65f693636a37945154994e562811c1617edeb0c
TRECN <rishabh1103@outlook.com>
2021-09-10 14:37:06 +0530

    commit

--- leap.cpp
+++ leap.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main(){
        int year = 2020;
-   if (year % 4 == 0) {
-      if (year % 100 == 0) {
-         if (year % 400 == 0)
-         cout << year << " is a leap year";
-         else
-         cout << year << " is not a leap year";
-      } else
-      cout << year << " is a leap year";
-   } else
-   cout << year << " is not a leap year";
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0)
+                    cout << year << " is a leap year";
+                else
+                    cout << year << " is not a leap year";
+            } else
+                cout << year << " is a leap year";
+        } else
+            cout << year << " is not a leap year";
    return 0;
 }
