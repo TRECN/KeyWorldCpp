24d3e3af67921cb2e5561c63aa7b26d9dfd743a4
TRECN <rishabh1103@outlook.com>
2021-09-10 20:38:58 +0530

    commit

---
+++ Lapin.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string a;
+        int m;
+        getline(cin,a);
+        if(s.length()%2==0)
+            m=s.length()/2;
+        else
+            m=(s.length()/2)+1;
+        vector<int> m1;
+        for(int i=0;i<m;i++){
+            char c=
+        }
+    }
+    return 0;
+}
