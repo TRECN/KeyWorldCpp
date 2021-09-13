40ed558c8192da8bc8b7cc9a536c8a1c2af97959
TRECN <rishabh1103@outlook.com>
2021-09-13 12:44:41 +0530

    commit

--- access.c
+++ access.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
-int factorial(int);
-int main(){
-    int a=5;
-    printf("%d",factorial(a));
-    return 0;
-}
 int factorial(int a){
     int fac=1;
     for (int i=1; i<=a;i++){
         fac=fac*i;
     }
     return fac;
+}
+int main(){
+    int a=5;
+    printf("%d",factorial(a));
+    return 0;
 }
