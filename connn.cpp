5b78ac63e5fd5015d4ea0bc76e81f3039509414a
TRECN <rishabh1103@outlook.com>
2021-09-14 10:34:31 +0530

    commit

---
+++ connn.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s,r;
+    cin>>s;
+    r=s;
+    cout<<"copy: "<<r<<endl;
+    cout<<"concatination: "<<s+r;
+    return 0;
+}
