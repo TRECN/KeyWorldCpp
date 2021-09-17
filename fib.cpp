43f58df703759e7cbb2ea47ec44e135b18fae64c
TRECN <rishabh1103@outlook.com>
2021-09-17 21:34:28 +0530

    commit

--- fib.cpp
+++ fib.cpp
@@ -11,6 +11,8 @@ int main(){
     for(int i=2;i<n;i++){
         a[i]=a[i-1]+a[i-2];
     }
-    for(int i=0;i<n;i++){}
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<endl;
+    }
     return 0;
 }
