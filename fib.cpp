ace0ec48ac5dfe8c2af439ce8f2817ee42294b50
TRECN <rishabh1103@outlook.com>
2021-09-17 21:15:51 +0530

    commti

---
+++ fib.cpp
@@ -0,0 +1,8 @@
+//write a program to generate n number of terms of fibonacci series using arry
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"enter the number of elements in fibonacci series: ";
+    
+    return 0;
+}
