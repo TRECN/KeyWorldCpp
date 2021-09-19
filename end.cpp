c87c01a556377a13be54de93e60d660478521f27
TRECN <rishabh1103@outlook.com>
2021-09-19 23:03:07 +0530

    commit

--- end.cpp
+++ end.cpp
@@ -1,15 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int ub;
-    cout<<"enter the number of element in the array";
-    cin>>ub;
+    int n,;
+    cout<<"enter the size of array";
+    cin>>n;
+
+    
     int a[ub+1];
     for(int i=0;i<ub;i++){
         cin<<a[ub];
     }
-
-
 
     int ud=n;
     cout<<"enter the element to insert: ";
