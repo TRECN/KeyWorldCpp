2e9ac7d149d7502508a013bc2e9cc56942d5d12c
TRECN <rishabh1103@outlook.com>
2021-09-19 23:39:47 +0530

    commmit

---
+++ bigin.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,ub,data;
+    cout<<"enter the size of array: "<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"enter the elements you want to enter: "<<endl;
+    cin>>ub;
+    ub=ub-1;
+    if(ub==n)
+        cout<<"ourflow!!";
+    else{
+        for(int i=0;i<=ub;i++){   
+            cin>>a[i];
+        }
+        int k=ub;
+        cout<<"Enter the data to insert:";
+        cin>>data;
+        ub=ub+1;
+        a[ub]=data;
+    }
+    cout<<"array after insertion: ";
+    for(int i=0;i<=ub;i++){
+        cout<<a[i]<<endl;
+    }
+    return 0;
+}
