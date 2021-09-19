f53783f96f7656a2738d810cf1d19bfdd22961b0
TRECN <rishabh1103@outlook.com>
2021-09-19 22:58:01 +0530

    commit

--- end.cpp
+++ end.cpp
@@ -4,8 +4,8 @@ int main(){
     int ub;
     cout<<"enter the number of element in the array";
     int a[ub+1];
-    for(int i=0;i<n;i++){
-        cin<<[]
+    for(int i=0;i<ub;i++){
+        cin<<a[ub];
     }
     int ud=n;
     cout<<"enter the element to insert: ";
