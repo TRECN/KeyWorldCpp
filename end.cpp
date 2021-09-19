9e367657d173a031401bb23ac6ce687f0da8c28e
TRECN <rishabh1103@outlook.com>
2021-09-19 22:46:09 +0530

    commit

--- end.cpp
+++ end.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 int main(){
     int n;
-    int a[]={1,2,3,4,45,12,6,12,5,10};
-    for(int i=0;i<n;i++){
-        cout<<a[i];
-    }
     
+    for(int i=0;i<n;i++){
+        
+    }
+    int a[];
+
+
     int ud=n;
     cout<<"enter the element to insert: ";
     n++;
