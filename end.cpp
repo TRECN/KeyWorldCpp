7adc33778e62ad3573259a6c7cc402993753a85a
TRECN <rishabh1103@outlook.com>
2021-09-19 23:19:09 +0530

    commit

--- end.cpp
+++ end.cpp
@@ -7,22 +7,19 @@ int main(){
     int a[n];
     cout<<"enter the elements you want to enter";
     cin>>ub;
-    
-    for(int i=0;i<ub;i++){
-        cin<<a[ub];
-    }
-
     if(ub==n)
         cout<<"ourflow!!";
     else{
-        
+        for(int i=0;i<ub;i++){
+        cin<<a[ub];
         cout<<"Enter the data to insert:";
         cin>>data;
         ub+=1;
         a[ub]=data;
     }
-    int ud=n;
-    cout<<"enter the element to insert: ";
-    n++;
+    cout<<"array after insertion: ";
+    for(int i=0;i<ub;i++){
+        cout<<
+    }
     return 0;
 }
