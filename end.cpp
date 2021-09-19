75efc88ffa064da10a02d8646924206a8b857fd7
TRECN <rishabh1103@outlook.com>
2021-09-19 22:41:51 +0530

    commit

--- end.cpp
+++ end.cpp
@@ -4,9 +4,10 @@ int main(){
     int a[]={1,2,3,4,45,12,6,12,5,10};
     
     int n=10;
-
+    int ud=n;
     cout<<"enter the element to insert: ";
-    
+    n++;
+
 
     for(int i=0;i<5;i++){
         cout<<a[i];
