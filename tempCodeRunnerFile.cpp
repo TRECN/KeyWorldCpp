8fcbc7bfd4b624ce167ea25a9249b2c0d73f3511
TRECN <rishabh1103@outlook.com>
2021-09-20 22:02:19 +0530

    commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,31 +1,18 @@
 #include<iostream>
-#include<cstdlib>
 using namespace std;
 int main(){
-    int t,n;
-    cin>>t;
-    while(t--){
-        cin>>n;
-        int a[n];
-        for(int k=0;k<n;k++){
-            cin>>a[k];
-        }
-        int i=0;
-        while(i<n-1){
-            int j=abs(a[i]%2);
-            int l=abs(a[i+1]%2)
-            if(j!=l){
-                int t=a[i];
-                a[i]=a[i+1];
-                a[i+1]=t;
-                
-            }
-            i++;
-        }
-        for(int k=0;k<n;k++){
-            cout<<a[k]<<" ";
-        }
-       
+    int n,r,s=0,t;
+    cout<<"enter a number: ";
+    cin>>n;
+    t=n;
+    while(n>0){
+        r=n%10;
+        s=(s*10)+r;
+        n=n/10;
     }
+    if(t==n)
+        cout<<"Palindrome";
+    else
+        cout<<"not Plindrome";
     return 0;
 }
