4ea3077443bd00c5359c1e268e339abefdb67840
TRECN <rishabh1103@outlook.com>
2021-09-20 22:00:36 +0530

    commit

--- clas.cpp
+++ clas.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 class a{
     public:
-        int sum(int a,int b){
-            return a+b;
+        int multi(int a,int b){
+            return a*b;
         }
 };
 int main(){
     a obj;
-    cout<<obj.sum(2,5)<<endl;
+    cout<<obj.multi(2,5)<<endl;
     return 0;
 }
