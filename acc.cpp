d0e15f250bf665118dbb13ae65f290dd37847e8a
TRECN <rishabh1103@outlook.com>
2021-09-21 10:10:07 +0530

    commit

---
+++ acc.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Student {
+    private:
+        int age;
+    public:
+        string name,branch;
+};
+int main(){
+    Student obj;
+    cin>>obj.name;
+    cin>>obj.branch;
+    cin>>obj.age;
+
+
+}
