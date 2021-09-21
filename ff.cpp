65d5a9a12ebf698da61c62bbaeb48f4a6598cec6
TRECN <rishabh1103@outlook.com>
2021-09-21 09:53:29 +0530

    commit

--- ff.cpp
+++ ff.cpp
@@ -1,9 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
-class MyClass {
+class Student {
     public:
-
-}
+    string name,branch;
+    int age;
+    Student(string n, int a, string b){
+        name=n;
+        age=a;
+        branch=b;
+    }
+};
 int main(){
-    MyClass obj;
+    string name,branch;
+    int age;
+    cout<<"enter student name, age, branch: "<<endl;
+    cin>>name;
+    cin>>age;
+    cin>>branch;
+    Student obj(name,age,branch);
+    cout<<"details: "<<endl;
+    cout<<obj.name<<endl<<obj.age<<endl<<obj.branch;
 }
