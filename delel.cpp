9f6423666660f3c99ffa4f9d95fbc9da7a5fac4e
TRECN <rishabh1103@outlook.com>
2021-09-22 14:43:49 +0530

    commit

---
+++ delel.cpp
@@ -0,0 +1,36 @@
+//at the begin
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,ub,data,lb=0;
+    cout<<"enter the size of array: "<<endl;
+    cin>>n;
+    int a[n];
+    cout<<"enter the elements you want to enter: "<<endl;
+    cin>>ub;
+    cout<<"enter elements: ";
+    ub=ub-1;
+    if(n==0)
+        cout<<"Underflow!!";
+    else{
+        for(int i=0;i<=ub;i++){   
+            cin>>a[i];
+        }
+        int x=lb;
+        cout<<"Enter the data to delete:";
+        cin>>data;
+        while(a[x]!=data){
+            x=x+1;
+        }
+        while(x<ub){
+            a[x]=a[x+1];
+            x=x+1;
+        }
+        ub=ub-1;
+    }
+    cout<<"array after insertion: "<<endl;
+    for(int i=0;i<=ub;i++){
+        cout<<a[i]<<endl;
+    }
+    return 0;
+}
