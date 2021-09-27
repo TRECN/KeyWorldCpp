c07090534244a603233ed5843d3ac4c2c4f04dba
TRECN <rishabh1103@outlook.com>
2021-09-27 12:45:07 +0530

    commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,18 +1,1 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int n,r,s=0,t;
-    cout<<"enter a number: ";
-    cin>>n;
-    t=n;
-    while(n>0){
-        r=n%10;
-        s=(s*10)+r;
-        n=n/10;
-    }
-    if(t==n)
-        cout<<"Palindrome";
-    else
-        cout<<"not Plindrome";
-    return 0;
-}
+rishabh
