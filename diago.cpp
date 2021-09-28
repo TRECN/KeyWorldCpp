bff6377d298d7c8d385f35392f1dd3fc708d6449
TRECN <rishabh1103@outlook.com>
2021-09-28 18:26:44 +0530

    commit

--- diago.cpp
+++ diago.cpp
@@ -4,5 +4,10 @@ int main(){
     int m,n;
     cout<<"enter the no. of rows and column: "<<endl;
     cin>>m>>n;
-    
+    int a[m][n];
+    for (int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
 }
