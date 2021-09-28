9ac62ea8fd5c10a0abf8676bf51929fe05ea0c9d
TRECN <rishabh1103@outlook.com>
2021-09-28 18:34:33 +0530

    commit

--- diago.cpp
+++ diago.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int m,n,ud,ld;
+    int m,n,ud,ld=0;
     cout<<"enter the no. of rows and column: "<<endl;
     cin>>m>>n;
     int a[m][n];
@@ -13,9 +13,10 @@ int main(){
     for (int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             if(i==j){
-                ud=a[i][j];
+                ud+=a[i][j];
             }
         }
     }
+    cout<<"sum: "<<ud;
 
 }
