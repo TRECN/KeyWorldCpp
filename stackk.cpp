d3fcbd84f73b8b039e4dc339df7c9dd360f6e2ae
TRECN <rishabh1103@outlook.com>
2021-09-29 22:52:29 +0530

    commit

--- stackk.cpp
+++ stackk.cpp
@@ -29,12 +29,7 @@ void pop(int *arr,int *top,int n){
 
 
 
-void show(int *arr,int *top,int n){
-    for(int i=0;i<=*top;i++){
-        cout<<arr[i]<<" ";
-    }
-    cout<<endl;
-}
+
 
 int main(){
     int n;
