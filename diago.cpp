d8d3430401df9ed50ac8c4d8ab1dfaf054aeba03
TRECN <rishabh1103@outlook.com>
2021-09-29 11:11:18 +0530

    commit

--- diago.cpp
+++ diago.cpp
@@ -1,3 +1,12 @@
+/*
+1  2  3  4
+1  2  3  4
+1  2  3  4
+1  2  3  4
+*/
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -6,17 +15,23 @@ int main(){
     cin>>m>>n;
     cout<<"enter the elements of the array: "<<endl;
     int a[m][n];
+    int n1=n;
     for (int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             cin>>a[i][j];
         }
     }
     for (int i=0;i<m;i++){
-        for(int j=0;j<n-1;j++){
+        for(int j=0;j<n1-1;j++){
             ud+=a[i][j];
         }
-        n--;
+        n1--;
     }
-    
+    for(int i=m-1,k=0;i>=0;i--,k++){
+        for(int j=n-1; j > k; j--){
+            ld+=a[i][j];
+        }
+    }
+
     cout<<"sum of ud elements : "<<ud<<endl;
 }
