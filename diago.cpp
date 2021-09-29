ceb9bc88bd27d2041531b882a4b8ca1b69127dac
TRECN <rishabh1103@outlook.com>
2021-09-29 11:03:04 +0530

    commit

--- diago.cpp
+++ diago.cpp
@@ -12,15 +12,11 @@ int main(){
         }
     }
     for (int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(i==j){
-                ud+=a[i][j];
-            }
+        for(int j=0;j<n-1;j++){
+            ud+=a[i][j];
         }
+        n--;
     }
-    for (int i=0,j=n-1;i<m;i++,j--){
-        ld+=a[i][j];
-    }
+    
     cout<<"sum of ud elements : "<<ud<<endl;
-    cout<<"sum of ld elements: "<<ld;
 }
