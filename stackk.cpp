044eb6319397a17870d275adf454534946b52866
TRECN <rishabh1103@outlook.com>
2021-09-29 22:51:43 +0530

    commit

--- stackk.cpp
+++ stackk.cpp
@@ -27,7 +27,15 @@ void pop(int *arr,int *top,int n){
     }
 }
 
-
+void pop(int *arr,int *top,int n){
+    int data;
+    if(*top<0)
+        cout<<"underflow!!"<<endl;
+    else{
+        arr[*top]=NULL;
+        *top=*top-1;
+    }
+}
 
 void show(int *arr,int *top,int n){
     for(int i=0;i<=*top;i++){
