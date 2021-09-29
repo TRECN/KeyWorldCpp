bfe27a1cd93ea626afa0bc09042cb7cc61ffefb5
TRECN <rishabh1103@outlook.com>
2021-09-29 09:56:34 +0530

    commit

---
+++ neww.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int *ptr=new int();
+    cin>>*ptr;
+    cout<<"value: "<<*ptr<<endl;
+    cout<<"address: "<<ptr<<endl;
+    return 0;
+}
