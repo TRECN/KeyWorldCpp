b906f92bcf90f1093f97b57e206e12ad304b349b
TRECN <rishabh1103@outlook.com>
2021-09-30 22:30:17 +0530

    commit

--- qualprel.cpp
+++ qualprel.cpp
@@ -13,6 +13,11 @@ int main(){
         }
         int f = sizeof(a) / sizeof(a[0]);
         sort(a, a + f, greater<int>());
+        for(int i=0;i<n;i++){
+            if(a[i]>=a[k])
+                c++;
+        }
+        cout<<c<<endl;
         
     }
     return 0;
