36eea391b5a5f950153abc997e1c21ba9be9a9e1
TRECN <rishabh1103@outlook.com>
2021-09-30 22:28:40 +0530

    commit

--- cheD.c
+++ cheD.c
@@ -4,7 +4,12 @@ void palind_Rev_sum(int a){
     int n=a;
     int r=0,s=0,sum;
     bool t=false;
-    
+    while(n!=0){
+        r=n%10;
+        s=s*10+r;
+        sum+=r;
+        n=n/10;
+    }
     printf("sum: %d\n",sum);
     printf("reverse: %d\n",s);
     if(s==a){
