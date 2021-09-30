68ada664f907f9a73b2c69d25a354e8b5c104016
TRECN <rishabh1103@outlook.com>
2021-09-30 22:19:41 +0530

    commit

--- ffff.cpp
+++ ffff.cpp
@@ -5,12 +5,7 @@ int main()
 {
 	int  n,s,t,i,m,k,lbA,ubB,d,j,c[7],lbB,ubA;
 	int a[5],b[6];
-	cout<<"enter the size:"<<endl;
-	cin>>n;
-	cout<<"enter the elements of array1:"<<endl;
-	cin>>s;
-	cout<<"enter the elements of array2:"<<endl;
-	cin>>t;
+	
 
 	i=lbA;
 	k=lbB;
