813b8cc6ebfaaaf42417cb5f7caf56e26bfa3aa6
TRECN <rishabh1103@outlook.com>
2021-09-30 13:52:37 +0530

    commit

---
+++ cheD.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdbool.h>
+void palind_Rev_sum(int a){
+    int n=a;
+    int r=0,s=0,sum;
+    bool t=false;
+    while(n!=0){
+        r=n%10;
+        s=s*10+r;
+        sum+=r;
+        n=n/10;
+    }
+    printf("sum: %d\n",sum);
+    printf("reverse: %d\n",s);
+    if(s==a){
+        printf("%d is palindrome.",a);
+    }
+    else
+        printf("%d is not palindrome.",a);
+}
+int main(){
+    int a;
+    printf("enter any no: \n");
+    scanf("%d",&a);
+    palind_Rev_sum(a);
+}
