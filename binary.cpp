83cfd012eff80d49ab8537084e6be8a57a2a40ec
TRECN <rishabh1103@outlook.com>
2021-10-01 22:42:41 +0530

    commit

---
+++ binary.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    bool c=false;
+    cout<<"enter the length of array: ";
+    cin>>n;
+    int begin=0,end=n-1,el,mid,i;
+    
+    int a[n];
+    cout<<"enter the elements: "<<endl;
+    for(i=0;i<n;i++)
+        cin>>a[i];
+    cout<<"enter the search element: "<<endl;
+    cin>>el;
+    while(begin<=end){
+        mid = begin + (end-begin)/2;
+        if (a[mid] == el){
+            c=true;
+            break;
+        }  
+        else if (a[mid] < el)
+            begin = mid + 1;
+        else
+            end = mid - 1;
+    }
+    if(c==true){
+        cout<<mid;
+    }
+    else
+        cout<<"not found";
+    return 0;
+}
