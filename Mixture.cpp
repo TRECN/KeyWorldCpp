3cee3248297bd5638f6fcdaca537c8ba7e2deea0
TRECN <rishabh1103@outlook.com>
2021-10-02 23:12:47 +0530

    commit

--- Mixture.cpp
+++ Mixture.cpp
@@ -6,7 +6,9 @@ int main(){
     while(t--){
         int a,b;
         cin>>a>>b;
-        
+        if(a>0&&b>0)
+            cout<<"Solution"<<endl;
+        else if(b==0&&a>0)
             cout<<"Solid"<<endl;
         else if(a==0&&b>0)
             cout<<"Liquid"<<endl;
