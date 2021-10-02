ea6353baf18cf0be936787d799e761336293c3a7
TRECN <rishabh1103@outlook.com>
2021-10-02 23:44:49 +0530

    commit

--- Threebox.cpp
+++ Threebox.cpp
@@ -8,18 +8,7 @@ int main(){
         int count;
         int a,b,c,d;
         cin>>a>>b>>c>>d;
-        if((a+b+c)==d){
-            count=1;
-        }
-        if(a==d){
-            count=3;
-        }
-        if((a+b+c)<d)
-            count=1;
-        if((d-(a+b+c))<0&&(a+b)<d)
-            count=2;
-        if((a+b+c)>(2*d))
-            count=3;
+        
         cout<<count;
         
         
