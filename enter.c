3efcdad760f764f07ef8f66d4b5b54bf2cfa727f
TRECN <rishabh1103@outlook.com>
2021-10-02 22:51:04 +0530

    commit

---
+++ enter.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter size of array: \n");
+    scanf("%d",&n);
+    int a[n];
+    printf("enter elements of array: \n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++){
+        printf("%d\n",a[i]);
+    }
+}
