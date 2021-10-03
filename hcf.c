310c1caa39f3690e00fffc2feb5e2338a5789ee0
TRECN <rishabh1103@outlook.com>
2021-10-03 20:55:50 +0530

    commit

---
+++ hcf.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+int  main()
+{
+	int i,num1,num2,hcf,mn;
+	
+	printf("enter the two nunbers\n");
+	scanf("%d%d",&num1,&num2);
+	
+	 if(num2>num1)
+      {
+      	mn=num2;
+      }
+      else
+      {
+       mn=num1;
+      }
+      for(i=mn;i>0;i--){
+         if(num1%i==0 && num2%i==0){
+             break;
+         }
+      }
+      printf("the hcf of these two number is %d\n",i);
+         return 0;
+}
