5b779f40b986ca3777cf97337b502a98dfc24898
TRECN <rishabh1103@outlook.com>
2021-10-04 17:02:03 +0530

    commit

---
+++ vectors.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int>a(10);
+    for(int i=0;i<5;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<5;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
