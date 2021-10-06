a462f4e4b0375fcd2f6306edbe03dd671cf7fce5
TRECN <rishabh1103@outlook.com>
2021-10-06 22:12:54 +0530

    commit

---
+++ hierar.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+class cars{
+    public:
+        string company="Car";
+};
+
+class car1:public cars{
+    public:
+        car1(int no){
+            cout<<no<< " ";
+            cout<<company<<endl;
+            
+        }
+
+};
+
+class car2:public cars{
+    public:
+        car2(int no){
+            cout<<no<<" ";
+            cout<<company<<endl;
+            
+        }
+
+};
+class car3:public cars{
+    public:
+        car3(int no){
+            cout<<no<<" ";
+            cout<<company<<endl;
+            
+        }
+
+};
+
+
+int main(){
+    car1 ob(1);
+    car2 ob1(2);
+    car3 ob2(2);
+    return 0;
+}
