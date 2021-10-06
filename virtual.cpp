a7b40114d3aaea848d47f57e75b4ff3390ea5968
TRECN <rishabh1103@outlook.com>
2021-10-06 23:00:57 +0530

    commit

---
+++ virtual.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class A{
+    public:
+        int a;
+};
+
+class B:public virtual A{
+    public:
+        void display(){
+            cout<<a;
+        }
+};
+class C:public virtual A{
+    public:
+        void display(){
+            cout<<a;
+        }
+};
+
+class D:public B,public C{
+    public:
+        B::display();
+        C::display();
+};
+
+
+int main(){
+    D obj;
+    obj.a=10;
+}
