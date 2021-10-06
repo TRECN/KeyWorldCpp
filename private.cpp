f08fae4722e5b261b973622d242d47c921e0e1ab
TRECN <rishabh1103@outlook.com>
2021-10-06 23:00:29 +0530

    commit

---
+++ private.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class a{
+    private:
+        int x=10;
+    public:
+        int y=20;
+    protected:
+        int z=30;
+
+        
+};
+class b:private a{
+    private:
+        int w=y+z;
+    
+    public:
+    b(){
+        cout<<w;
+    }
+};
+
+            
+int main(){
+    b B;
+}
