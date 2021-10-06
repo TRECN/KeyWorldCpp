71a1453f898a8dca1cad91f6b1528fe25a5bee98
TRECN <rishabh1103@outlook.com>
2021-10-06 23:00:46 +0530

    commit

---
+++ trial.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[n];//1 5 3 6 3 4 2 1 6 
+    } 
+    for(int i=0;i<n;i++){
+        cout<<a[n];//1 5 3 6 3 4 2 1 6 
+    } 
+    
+    return 0;
+}
