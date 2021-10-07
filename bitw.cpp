ffd3622e2baa65de4f7df400883a3e360fb22b23
TRECN <rishabh1103@outlook.com>
2021-10-07 21:59:28 +0530

    commit

--- bitw.cpp
+++ bitw.cpp
@@ -1,6 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int a=;
-    cout<<a;
+    long long int t;
+    cin>>t;
+    while(t--){
+        long long int n,st;
+        cin>>n;
+        if(n==1){
+            cout<<1;
+            continue;
+        }
+        long long int temp=1;
+        while(temp*2<=n){
+            temp*=2;
+        }
+        st=n-temp+1;
+        if(n==temp)
+            cout<<temp/2;
+        else
+            cout<<max(st,temp/2);
+    }
 }
