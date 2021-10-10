cf7e296698c94e5501b91fbccd4e2c6507998e3a
TRECN <rishabh1103@outlook.com>
2021-10-10 20:09:19 +0530

    commit

---
+++ bitOne.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int numSetBits(unsigned int A){
+    int count=0;
+    
+    while(A!=0){
+        A=A&(A-1);
+        count++;     
+    }
+    return count;
+}
+
+int main(){
+    cout<<numSetBits(5);
+    return 0;
+}
