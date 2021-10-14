354963feb5421d830ead5b70cc40a8ee3253cb87
TRECN <rishabh1103@outlook.com>
2021-10-14 22:55:53 +0530

    commit

--- enter.c
+++ enter.c
@@ -5,9 +5,7 @@ int main(){
     scanf("%d",&n);
     int a[n];
     printf("enter elements of array: \n");
-    for(int i=0;i<n;i++){
-        scanf("%d",&a[i]);
-    }
+    
     for(int i=0;i<n;i++){
         printf("%d\n",a[i]);
     }
