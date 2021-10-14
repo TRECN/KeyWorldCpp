3c7644709f8d3b8414a7b24c4cdf1c9b1ed0dccf
TRECN <rishabh1103@outlook.com>
2021-10-14 23:02:53 +0530

    commit

--- ffff.cpp
+++ ffff.cpp
@@ -29,13 +29,6 @@ int main()
 	    k=k+1;
 	}
 	cout<<"after concatenation:"<<endl;
-	for(i=0;i<=s;i++)
-	{
-       cout<<"a[i]=s"<<endl;
-    }
-    for( j=0;j<=t;j++)
-    {
-    	cout<<"a[j]=s"<<endl;
-    }
+
     return 0;
 }
