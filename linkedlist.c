f6ff099fa595c99c884f3a6e35c9ff9137453484
TRECN <rishabh1103@outlook.com>
2021-10-31 22:08:36 +0530

    commit

--- linkedlist.c
+++ linkedlist.c
@@ -3,6 +3,14 @@ struct node{
     int data;
     struct node* next;
 };
+
+void create(){
+    char ch;
+    struct node* p,*q;
+    p=(struct node*)malloc(sizeof(struct node));
+    
+}
+
 void main(){
 
 }
