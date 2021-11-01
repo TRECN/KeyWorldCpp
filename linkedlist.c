4cc67ac479530baf27f6899584348043bcd18a7e
TRECN <rishabh1103@outlook.com>
2021-11-01 22:49:23 +0530

    commit

--- linkedlist.c
+++ linkedlist.c
@@ -10,24 +10,19 @@ struct node* start;
 
 
 
-
-
-
-void inend(){
-    struct node *p,*q;
-    q=(struct node*)malloc(sizeof(struct node));
-    printf("enter data: ");
-    scanf("%d",&q->data);
-    p=start;
-    while(p->next!=NULL){
-        p=p->next;
-    }
-        q->next=NULL;
-        p->next=q;
-
+void create(){
+    char ch='y';
+    printf("linked list created!!\nenter first element:\n");
+    struct node* p,*q;
+    p=(struct node*)malloc(sizeof(struct node));
+    scanf("%d",&p->data);
+    p->next=NULL;
+    start=p;  
     printf("---------\n\n");
-        
 }
+
+
+
 
 void inbeg(){
     struct node* p;
