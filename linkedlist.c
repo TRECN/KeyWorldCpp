95000acfe801c0ad2c85678238f7631f63595e45
TRECN <rishabh1103@outlook.com>
2021-11-01 13:38:14 +0530

    commit

--- linkedlist.c
+++ linkedlist.c
@@ -16,16 +16,8 @@ void create(){
     struct node* p,*q;
     p=(struct node*)malloc(sizeof(struct node));
     scanf("%d",&p->data);
-    start=p;
-    do{
-        q=(struct node*)malloc(sizeof(struct node));
-        scanf("%d",&q->data);
-        p->next=q;
-        p=q;
-        printf("do you want to create node again(y/n)");
-        ch=getch();
-    }while(ch=='y');
-       p->next=NULL;
+    p->next=NULL;
+    start=p;  
     printf("---------\n\n");
 }
 
