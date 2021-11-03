554eff997b33fba5d416d8e63f84d131325ee4f4
TRECN <rishabh1103@outlook.com>
2021-11-03 11:50:11 +0530

    commit

--- linkedlist.c
+++ linkedlist.c
@@ -109,7 +109,7 @@ void main(){
 
 
     while(ch>0){
-        printf("1. insert at end\n2. traverse\n3. insert in begin\n4. at given value\n0. to exit\n");
+        printf("1. insert at end\n2. traverse\n3. insert in begin\n4. after given value\n5. before given value\n0. to exit\n");
         
         scanf("%d",&ch);
         printf("---------\n\n");
