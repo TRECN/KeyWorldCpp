8ae31f3e6afae0081a04cff08fe8b1b564338b8c
TRECN <rishabh1103@outlook.com>
2021-11-04 21:30:48 +0530

    commit

--- linkedlist.c
+++ linkedlist.c
@@ -73,7 +73,6 @@ void befgivenVal(){
     printf("enter the data before which you want add a node: ");
     scanf("%d",&x);
     printf("enter the value: ");
-    scanf("%d",&q->data);
     while(p->next->data!=x){
         p=p->next;
     }
