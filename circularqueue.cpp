11130723a8735200392fee742e233878e026e6c4
TRECN <rishabh1103@outlook.com>
2021-11-07 21:11:32 +0530

    commit

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -13,6 +13,8 @@ void insertion(char *q,int n,int *front,int *rear){
             *front=0;
             *rear=0;
         }
+        if(rear==n-1)
+            rear=0;
         else
             *rear=*rear+1;
 
