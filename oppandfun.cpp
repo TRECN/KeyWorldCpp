4fdf39563ba722e7f61e5aebd6f25ee6d4c5b27f
TRECN <rishabh1103@outlook.com>
2021-11-09 11:54:38 +0530

    comimt

---
+++ oppandfun.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+class A{
+    
+    public:
+        int a;
+        int b;
+        A(){}
+        A(int a1,int b1){
+            a=a1;
+            b=b1;
+        }
+
+        friend A operator+= (A j,A i){
+            A k;
+            k.a=j.a*i.a;
+            k.b=j.b*i.b;
+            return (k);
+        }
+        virtual void show(A a){
+            
+        }
+        void show(){
+            cout<<a<<"\t"<<b<<"\n";
+        }
+};
+class B : public A{
+    public:
+        void show(A a){
+            a.show();
+        }
+        
+};
+int main(){
+    A *ptr;
+    B ptr1;
+    ptr=&ptr1;
+
+    A o1(3,2);
+    A o2(2,2);
+    A o3;
+    
+    cout<<"showing values in o1 and o2 using o3 as function overloading\n";
+    ptr->show(o1);
+    ptr->show(o2);
+    cout<<"operator overloading\n";
+    ptr->show(o1);
+    cout<<"    X"<<endl;
+    ptr->show(o2);
+    cout<<"---------"<<endl;
+    o3 = o1 += o2;
+    ptr->show(o3);
+
+    
+    
+    return 0;
+}
