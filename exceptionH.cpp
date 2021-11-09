2d5cc5a72bcbf9a6f6dd23cdf37178dd0f909108
TRECN <rishabh1103@outlook.com>
2021-11-09 09:07:10 +0530

    commit

---
+++ exceptionH.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a = 10,b=0;
+    try{
+            a=a/;
+            cout<<a;
+    }
+    catch(exception e){
+        cout<<"denominator not be zero";
+    }
+}
