2d26ce5e6dab02d7773e7e64aa4ccac6e64e9174
TRECN <rishabh1103@outlook.com>
2021-11-17 21:07:07 +0530

    commit

--- switchc.cpp
+++ switchc.cpp
@@ -5,7 +5,29 @@ int main(){
     int n;
     cin>>n;
     switch(n){
-        
+        case 1:
+            cout<<"monday";
+        break;
+        case 2:
+            cout<<"tuesday";
+        break;
+        case 3:
+            cout<<"wednesday";
+        break;
+        case 4:
+            cout<<"thursday";
+        break;
+        case 5:
+            cout<<"friday";
+        break;
+        case 6:
+            cout<<"saturday";
+        break;
+        case 7:
+            cout<<"sunday";
+        break;
+        dafault:
+            cout<<"wrong choice!!";
     }
     return 0;
 }
