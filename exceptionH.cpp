fd81e2a8958287292c9c898bdc8a5681182ff78e
TRECN <rishabh1103@outlook.com>
2021-11-18 22:59:21 +0530

    commit

--- exceptionH.cpp
+++ exceptionH.cpp
@@ -3,7 +3,14 @@ using namespace std;
 int main(){
     int a,b;
     cin>>a>>b;
-    
+    try{
+        if(b==0)
+            throw b;
+        else if(a/b==0)
+            throw (float)a/b;
+        else
+            cout<<a/b;
+    }
     catch(int d){
         cout<<"dividing by 0\n";
     }
