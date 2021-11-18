1ff91b8437c5d8fc44ae474bee6d1f2d6c084316
TRECN <rishabh1103@outlook.com>
2021-11-18 22:58:00 +0530

    commit

--- exceptionH.cpp
+++ exceptionH.cpp
@@ -6,7 +6,10 @@ int main(){
     try{
         if(b==0)
             throw b;
-        
+        else if(a/b==0)
+            throw (float)a/b;
+        else
+            cout<<a/b;
     }
     catch(int d){
         cout<<"dividing by 0\n";
