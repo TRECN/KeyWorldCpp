c639f8b7a2dd2e1a16872d0e7db3750a3164fe20
TRECN <rishabh1103@outlook.com>
2021-11-18 22:52:19 +0530

    commit

--- linkedlist.c
+++ linkedlist.c
@@ -193,7 +193,10 @@ void main(){
                 inend();
             break;
 
-             
+            case 2:
+                printf("traversing...\n");
+                traverse();
+            break; 
             case 3:
                 printf("creating a node...\n");
                 inbeg();   
