99a9545ec2957bb49bb39b49cabeebc223a3c457
TRECN <rishabh1103@outlook.com>
2021-11-19 19:46:59 +0530

    commit

--- factorial.cpp
+++ factorial.cpp
@@ -3,9 +3,7 @@ using namespace std;
 int main(){
     int n,f=1;
     cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++)
-            f=f*j;
+    
 
         cout<<"Factorial of "<<i<<" = "<<f<<endl;
         f=1;
