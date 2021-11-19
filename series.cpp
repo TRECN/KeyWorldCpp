ab5807eb87fca3de2b6bb7b9934acdb7be9a780c
TRECN <rishabh1103@outlook.com>
2021-11-19 20:30:20 +0530

    commit

--- series.cpp
+++ series.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main(){
     float sum=0,f=0;;
-    for(int i=1;i<=2;i++){
+    for(int i=1;i<=100;i++){
         if(i%2==0)
-            sum=sum-(1/i);
+            sum-=(float)1/i;
         else
-            sum=sum+(1/i);
+            sum+=(float)1/i;
     }
     cout<<"sum: "<<sum;
 return 0;
