bfba8132f3c88afb8b53b0a5105c19499ac334fa
TRECN <rishabh1103@outlook.com>
2021-11-19 20:00:44 +0530

    commit

---
+++ sphere.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r;
+    float v,a;
+    cin>>r;
+    v=(4/3)*3.14*r*r*r;
+    a=4*3.14*r*r;
+    cout<<"area: "<<a<<endl;
+    cout<<"volume: "<<v<<endl;
+}
