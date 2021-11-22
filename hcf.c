9b8748a881eff2d58e31b36eddef08bbb9c5c33c
TRECN <rishabh1103@outlook.com>
2021-11-22 21:28:10 +0530

    commit

--- hcf.c
+++ hcf.c
@@ -7,14 +7,7 @@ int  main()
 	printf("enter the two nunbers\n");
 	scanf("%d%d",&num1,&num2);
 	
-	 if(num2>num1)
-      {
-      	mn=num2;
-      }
-      else
-      {
-       mn=num1;
-      }
+	 
       for(i=mn;i>0;i--){
          if(num1%i==0 && num2%i==0){
              break;
