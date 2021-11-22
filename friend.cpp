826044fd76b9afebdbe84f625fcec2e244c32669
TRECN <rishabh1103@outlook.com>
2021-11-22 21:33:01 +0530

    commit

--- friend.cpp
+++ friend.cpp
@@ -1,7 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+class A
+{
+  int a, b;
+  public:
+    void set()
+    {
+    a=50;
+    b=30;
+    }
+  friend int sum(A s);
+};
 
 int sum(A s)
 {
