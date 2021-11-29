3dc565cb5c6cbaae2a4c1995c97355946830d5e4
TRECN <rishabh1103@outlook.com>
2021-11-29 21:34:51 +0530

    commit

--- binary.c
+++ binary.c
@@ -1,30 +1,33 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include<stdio.h>
+#include<stdbool.h>
 int main(){
     int n;
     bool c=false;
-    cout<<"enter the length of array: ";
-    cin>>n;
-    int begin=0,end=n-1,el;
+    printf("enter the length of array: ");
+    scanf("%d",&n);
+    int begin=0,end=n-1,el,mid,i;
     
     int a[n];
-    cout<<"enter the elements: "<<endl;
+    printf("enter the elements: \n");
     for(i=0;i<n;i++)
-        cin>>a[i];
-    cout<<"enter the search element: "<<endl;
-    cin>>el;
+        scanf("%d",&a[i]);
+    printf("enter the search element: \n");
+    scanf("%d",&el);
     while(begin<=end){
-        int mid = begin + (end-begin)/2;
-      if (a[mid] == el){
-          c=true;
-          break;
-      }  
-      else if (a[mid] < el)
-         begin = mid + 1;
-      else
-         end = mid - 1;
+        mid = begin + (end-begin)/2;
+        if (a[mid] == el){
+            c=true;
+            break;
+        }  
+        else if (a[mid] < el)
+            begin = mid + 1;
+        else
+            end = mid - 1;
     }
     if(c==true){
-
+        printf("%d",mid);
     }
+    else
+        printf("not found");
+    return 0;
 }
