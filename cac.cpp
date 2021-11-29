c123228a3bc64847ba93e4030fcf81756acab8ca
TRECN <rishabh1103@outlook.com>
2021-11-29 21:39:07 +0530

    commit

---
+++ cac.cpp
@@ -0,0 +1,20 @@
+# include<iostream> 
+
+using namespace std;
+ 
+    int fact( int n)	 
+    {
+        if(n<=1)
+ 	        return 1;
+ 	    else 
+ 	        return n*fact(n-1);
+ 	}
+     
+int main(){
+ 	int n,f;
+ 	cout<<" Enter the number " ;
+ 	cin>>n;
+ 	f=fact(n);
+ 	cout<<f;
+    return 0;
+ }
