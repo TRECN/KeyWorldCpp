d41f152299806a98db80916f8e063cfae560df65
TRECN <rishabh1103@outlook.com>
2021-12-01 23:52:53 +0530

    commit

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -5,7 +5,9 @@ int main(){
     cin>>n;
     int a[n];
     for(int i=0;i<n;i++){
-   
+        cin>>a[i];
+    }
+
     for(int i=1;i<n;i++){
         for(int j=0;j<n-i;j++){
             if(a[j]>a[j+1]){
