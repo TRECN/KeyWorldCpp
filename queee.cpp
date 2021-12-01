3ee40fe75c6a444d67d4d427d3b895d5c1c522bd
TRECN <rishabh1103@outlook.com>
2021-12-01 23:51:11 +0530

    commit

--- queee.cpp
+++ queee.cpp
@@ -5,7 +5,10 @@ int main(){
     int n;
     cout<<"enter the size of queue: "<<endl;
     cin>>n;
-   
+    int q[n];
+    int front=-1;
+    int rear=-1;
+    int ch;
     do{
         if(front==-1&&rear==n-1){
             rear=-1;
