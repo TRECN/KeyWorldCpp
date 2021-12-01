04e9a0355f48ca6f3b85c3cc5091977f8b72c9cf
TRECN <rishabh1103@outlook.com>
2021-12-01 23:54:31 +0530

    commit

--- selectionSort1.cpp
+++ selectionSort1.cpp
@@ -22,18 +22,3 @@ void selectionSort(int *array, int size) {
          swap(array[i], array[imin]);
    }
 }
-int main() {
-   int n;
-   cout << "Enter the number of elements: ";
-   cin >> n;
-   int arr[n];           //create an array with given number of elements
-   cout << "Enter elements:" << endl;
-   for(int i = 0; i<n; i++) {
-      cin >> arr[i];
-   }
-   cout << "Array before Sorting: ";
-   display(arr, n);
-   selectionSort(arr, n);
-   cout << "Array after Sorting: ";
-   display(arr, n);
-}
