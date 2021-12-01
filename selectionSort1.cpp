2bca860053c18621dbab9e7353c699a0f2a18481
TRECN <rishabh1103@outlook.com>
2021-12-01 23:53:50 +0530

    commit

--- selectionSort1.cpp
+++ selectionSort1.cpp
@@ -1,11 +1,6 @@
 #include<iostream>
 using namespace std;
-void swapping(int &a, int &b) {         //swap the content of a and b
-   int temp;
-   temp = a;
-   a = b;
-   b = temp;
-}
+
 void display(int *array, int size) {
    for(int i = 0; i<size; i++)
       cout << array[i] << " ";
