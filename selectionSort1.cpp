75e00e700bc274d2cb78494a5acebc6b6e5be66a
TRECN <rishabh1103@outlook.com>
2021-12-02 21:28:14 +0530

    commit

--- selectionSort1.cpp
+++ selectionSort1.cpp
@@ -11,7 +11,17 @@ void display(int *array, int size) {
       cout << array[i] << " ";
    cout << endl;
 }
-
+void selectionSort(int *array, int size) {
+   int i, j, imin;
+   for(i = 0; i<size-1; i++) {
+      imin = i;   //get index of minimum data
+      for(j = i+1; j<size; j++)
+         if(array[j] < array[imin])
+            imin = j;
+         //placing in correct position
+         swap(array[i], array[imin]);
+   }
+}
 int main() {
    int n;
    cout << "Enter the number of elements: ";
