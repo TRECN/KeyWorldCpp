96764dbc8bd23901b90dfc4beb135943c64f9b28
TRECN <rishabh1103@outlook.com>
2021-12-03 20:32:11 +0530

    commit

--- reverseBitt.cpp
+++ reverseBitt.cpp
@@ -1,13 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-uint32_t numSetBits(uint32_t A){
 
-    uint32_t rev=0;
-    int mask;
-    
-    return rev;
-}
 
 int main(){
     cout<<numSetBits(3);
