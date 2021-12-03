ea304d07502a853969c983fee154fd9f31c0aebd
TRECN <rishabh1103@outlook.com>
2021-12-03 20:15:58 +0530

    commit

--- subarr.cpp
+++ subarr.cpp
@@ -12,3 +12,15 @@ void subar(int a[],int n){
     }
 }
 
+int main(){
+
+    int n;
+    
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    subar(a,n);
+    return 0;
+}
