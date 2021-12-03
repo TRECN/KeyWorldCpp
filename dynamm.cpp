36400fde6f819a653341b6a8da919d8ce6a9c9a8
TRECN <rishabh1103@outlook.com>
2021-12-03 20:18:55 +0530

    commit

--- dynamm.cpp
+++ dynamm.cpp
@@ -1,4 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+class S
+{
+    public:
+        static int a;
+        S (){
+            a++;
+            cout<<"constructor : "<<a<<endl;
+        }
 
+        ~S(){
+            a++;
+            cout<<"distructor : "<<a<<endl;
+            
+        }
+};
 int S::a=0;
 int main(){
     S *s=new S[3];
