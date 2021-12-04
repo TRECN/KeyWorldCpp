3344beb07f1bbf6ebad11cb678d84caeaa8a2eff
TRECN <rishabh1103@outlook.com>
2021-12-04 22:10:01 +0530

    commit

--- private.cpp
+++ private.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 
 class a{
+    private:
+        int x=10;
+    public:
+        int y=20;
+    protected:
+        int z=30;
 
+        
+};
 class b:private a{
     private:
         int w=y+z;
