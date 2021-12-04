861cfee2cb1d26f4d3c9f6429488d2f27ba2b5f0
TRECN <rishabh1103@outlook.com>
2021-12-04 22:07:56 +0530

    commit

--- funco.cpp
+++ funco.cpp
@@ -3,4 +3,11 @@ using namespace std;
 int sum(int a, int b){
     return a+b;
 }
-i
+int sum(int a){
+    return a+a;
+}
+int main(){
+    cout<<sum(25)<<endl;
+    cout<<sum(3,10);
+    return 0;
+}
