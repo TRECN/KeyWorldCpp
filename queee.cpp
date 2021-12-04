55e46ad6f226fc2a368a3ad241c03059dbd1543b
TRECN <rishabh1103@outlook.com>
2021-12-04 22:06:06 +0530

    commit

--- queee.cpp
+++ queee.cpp
@@ -41,7 +41,15 @@ int main(){
             case 2:
                 if(front==-1)
                     cout<<"underflow!!"<<endl;
-                
+                else{
+                    if(front==rear){
+                        q[front]=NULL;
+                        front=-1;
+                    }else{
+                        q[front]=NULL;
+                        front=front+1;
+                    }
+                }
             break;
 
             //show queue
