9461b81a75448915e8f6f0747d4c5d7bc04864e5
TRECN <rishabh1103@outlook.com>
2021-12-04 22:05:19 +0530

    commit

--- queee.cpp
+++ queee.cpp
@@ -25,7 +25,11 @@ int main(){
                     cout<<"enter data: "<<endl;
                     int data;
                     cin>>data;
-                    
+                    if(front==-1){
+                        front+=1;
+                        rear+=1;
+                        q[rear]=data;
+                    }
                     else{
                         rear+=1;
                         q[rear] = data;
