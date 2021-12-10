23b6d38880b1941f288abee64fd9dc402faad5c3
TRECN <rishabh1103@outlook.com>
2021-12-10 20:56:00 +0530

    commit

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -16,11 +16,7 @@ void merge(int arr[],int l,int mid, int r){
     int i=0;
     int j=0;
     int k=l;
-    while(i<n1&&j<n2){
-        if(a[i]<b[j]){
-            arr[k]=a[i];
-            k++;i++;
-        }
+ 
         else{
             arr[k]=b[j];
             k++;j++;
