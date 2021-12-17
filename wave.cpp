d931167ba12f973e53b55f5fe46d2e174565ef91
TRECN <rishabh1103@outlook.com>
2021-12-17 22:43:31 +0530

    commit

--- wave.cpp
+++ wave.cpp
@@ -1,3 +1,24 @@
-
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+        int a[n];
+        int l=1;
+        for(int k=0;k<n;k++){
+            cin>>a[k];
+        }
+        if(n%2!=0){
+            l=2;
+        }
+        for(int i=0;i<n-l;i+=2){
+            a[i]=a[i]+a[i+1];
+            a[i+1]=a[i]-a[i+1];
+            a[i]=a[i]-a[i+1]; 
+        }
+        for(int k=0;k<n;k++){
+            cout<<a[k]<<" ";
+        }
     return 0;
 }
