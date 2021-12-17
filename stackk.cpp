4da541bb194df92588a5cf0049360909668d0da8
TRECN <rishabh1103@outlook.com>
2021-12-17 22:44:31 +0530

    commit

--- stackk.cpp
+++ stackk.cpp
@@ -1,16 +1,5 @@
 #include<bits/stdc++.h>
 using namespace std;
-void push(int *arr,int *top,int n){
-    int data;
-    if(*top==n-1)
-        cout<<"overflow!!"<<endl;
-    else{
-        cout<<"enter element: "<<endl;
-        cin>>data;
-        *top=*top+1;
-        arr[*top]=data;
-    }
-}
 
 void pop(int *arr,int *top,int n){
     int data;
