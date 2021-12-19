44118f88b3d2814a9f37b21bbb13084dae21e262
TRECN <rishabh1103@outlook.com>
2021-12-19 22:36:14 +0530

    commit

--- MaximizeTheSum.cpp
+++ MaximizeTheSum.cpp
@@ -4,7 +4,13 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        
+        int n,k;
+        cin>>n>>k;
+        int s=0;
+        vector<int>vec(n);
+        for(int i=0;i<n;i++){
+            cin>>vec[i];
+        }
         sort(vec.begin(),vec.end());
         int h=n;
         while(k--){
