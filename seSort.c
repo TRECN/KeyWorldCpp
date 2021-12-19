c2217ab4118046c98f61f72a6fd6bf4c49735533
TRECN <rishabh1103@outlook.com>
2021-12-19 22:33:05 +0530

    commit

--- seSort.c
+++ seSort.c
@@ -11,7 +11,15 @@ void swap(int *xp, int *yp)
 
 void selectionSort(int arr[], int n)
 {
-    i
+    int i, j, min_idx;
+    for (i = 0; i < n-1; i++)
+    {
+        min_idx = i;
+        for (j = i+1; j < n; j++)
+          if (arr[j] < arr[min_idx])
+            min_idx = j;
+        swap(&arr[min_idx], &arr[i]);
+    } }
 void printArray(int arr[], int size)
 {
     int i;
