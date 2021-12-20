7d375c23606a6879e117cd74d54271ba912d6a72
TRECN <rishabh1103@outlook.com>
2021-12-20 21:56:24 +0530

    commit

--- heapSort.c
+++ heapSort.c
@@ -5,7 +5,22 @@
     *b = temp;
   }
   
-x
+  void heapify(int arr[], int n, int i) {
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+  
+    if (left < n && arr[left] > arr[largest])
+      largest = left;
+  
+    if (right < n && arr[right] > arr[largest])
+      largest = right;
+  
+    if (largest != i) {
+      swap(&arr[i], &arr[largest]);
+      heapify(arr, n, largest);
+    }
+  }
   
   void heapSort(int arr[], int n) {
     for (int i = n / 2 - 1; i >= 0; i--)
