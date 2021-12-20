5702b86151f7d2daef0eb7cf65fc496e9ca23b07
TRECN <rishabh1103@outlook.com>
2021-12-20 22:01:44 +0530

    commit

--- assignment4.cpp
+++ assignment4.cpp
@@ -22,3 +22,76 @@ void insertion(char *q,int n,int *front,int *rear,char data){
 
 
 
+void deletion(char *q,int n,int *front,int *rear){
+    if(*front==-1)
+        cout<<"queue is underflow!!";
+    else{
+        if(*front==*rear){
+            q[*front]=NULL;
+            *front=-1;
+            *rear=-1;
+        }
+        else if(*front==n-1){
+            q[*front]=NULL;
+            *front=0;
+        }
+        else{
+            q[*front]=NULL;
+            *front=*front+1;
+        }
+    }
+
+}
+
+void show(char *q,int n,int *front, int *rear){
+    if(*front!=-1){
+        cout<<"queue: ";
+        for(int i=0;i<=n;i++){
+            cout<<q[i]<<" ";
+        }
+    }
+    else{
+        cout<<"queue is empty";
+    }
+
+    cout<<endl<<"front: "<<*front<<endl<<"rear: "<<*rear<<endl;
+    cout<<endl;
+
+}
+
+int main(){
+    char q[6];
+    q[2]='A';
+    q[3]='C';
+    q[4]='D';
+    int front=2;
+    int rear=4;
+    insertion(q,6,&front,&rear,'F');
+    show(q,6,&front,&rear);
+    
+    deletion(q,6,&front,&rear);
+    deletion(q,6,&front,&rear);
+    show(q,6,&front,&rear);
+
+    insertion(q,6,&front,&rear,'K');
+    insertion(q,6,&front,&rear,'L');
+    insertion(q,6,&front,&rear,'M');
+    show(q,6,&front,&rear);
+
+    deletion(q,6,&front,&rear);
+    deletion(q,6,&front,&rear);
+    show(q,6,&front,&rear);
+
+    insertion(q,6,&front,&rear,'S');
+    show(q,6,&front,&rear);
+
+    deletion(q,6,&front,&rear);
+    show(q,6,&front,&rear);
+
+    insertion(q,6,&front,&rear,'R');
+    show(q,6,&front,&rear);
+
+    deletion(q,6,&front,&rear);
+    show(q,6,&front,&rear);
+
+}
