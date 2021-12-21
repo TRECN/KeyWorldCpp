7d613fbbe72b988dc4047122f1be251fd6877b9a
TRECN <rishabh1103@outlook.com>
2021-12-21 22:42:17 +0530

    commit

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -49,3 +49,12 @@ void mergeSort(int arr[],int l,int r){
 }
 
 
+int main(){
+    int arr[]={5,6,3,2,4};
+    mergeSort(arr,0,4);
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
