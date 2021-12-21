1b539d09bc4e01729941a4cadcaa47a82d9255a7
TRECN <rishabh1103@outlook.com>
2021-12-21 22:41:46 +0530

    commit

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -2,7 +2,17 @@
 using namespace std;
 
 void merge(int arr[],int l,int mid, int r){
-    
+    int n1=mid-l+1;
+    int n2=r-mid;
+
+    int a[n1];
+    int b[n2];
+    for(int i=0;i<n1;i++){
+        a[i]=arr[l+i];
+    }
+    for(int i=0;i<n2;i++){
+        b[i]=arr[mid+1+i];
+    }
     int i=0;
     int j=0;
     int k=l;
