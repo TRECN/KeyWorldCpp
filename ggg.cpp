af65c7b511dab384e8aa1ab69b9fb678295211eb
TRECN <rishabh1103@outlook.com>
2021-12-21 22:34:06 +0530

    commit

---
+++ ggg.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(int argc,char const *argv[]){
+char s1[6]="hello";
+char s2[6]="world";
+char s3[12]=s1 + " " + s2;
+cout<<s3;
+return 0;
+}
