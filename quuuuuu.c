ca2449320210509a12d9ffee41637989968c5916
TRECN <rishabh1103@outlook.com>
2021-12-22 22:40:26 +0530

    commit

--- quuuuuu.c
+++ quuuuuu.c
@@ -6,7 +6,27 @@ void insertion()
 {
    int val;
 
-   
+   if (rear == n - 1)
+   {
+      printf("Queue is Overflow\n");
+   }
+   else
+   {
+       printf("Insert the element\n");
+        scanf("%d", &val);
+      if (front == -1)
+      {
+         
+         front+=1;
+         rear = rear + 1;
+         Queue[rear] = val;
+      }
+      else
+      {
+          rear+=1;
+          Queue[rear]=val;
+      }
+   }
 }
 void deletion()
 {
