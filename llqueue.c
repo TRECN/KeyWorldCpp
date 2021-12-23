11e3115c04f65361202f4b6be73d7d474d1b48f2
TRECN <rishabh1103@outlook.com>
2021-12-23 22:57:50 +0530

    commit

--- llqueue.c
+++ llqueue.c
@@ -86,5 +86,14 @@ void display()
     {
         printf("\nEmpty queue");
     }
-    
+    else
+    {   
+        printf("Elements of Queue:");
+        while(ptr != NULL)
+        {
+            printf(" %d",ptr -> data);
+            ptr = ptr -> next;
+        }
+        printf("\n");
+    }
 }
