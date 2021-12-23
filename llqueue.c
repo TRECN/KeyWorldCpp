e11697d2102f671ad7b52fb7e858892834c0192e
TRECN <rishabh1103@outlook.com>
2021-12-23 22:56:33 +0530

    commit

--- llqueue.c
+++ llqueue.c
@@ -3,7 +3,14 @@
 struct node
 {
     int data;
-
+    struct node *next;
+};
+struct node *front;
+struct node *rear;
+void ins();
+void del();
+void display();
+void main ()
 {
     int choice;
     while(choice != 4){
