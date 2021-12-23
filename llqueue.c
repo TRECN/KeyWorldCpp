8785992f1cd15d722d33fc38ae7aad681ea57a8c
TRECN <rishabh1103@outlook.com>
2021-12-23 22:59:49 +0530

    commit

--- llqueue.c
+++ llqueue.c
@@ -46,24 +46,7 @@ void ins(){
         printf("\nOVERFLOW\n");
         return;
     }
-    else{
-        printf("Enter value:");
-        scanf("%d",&item);
-        ptr -> data = item;
-        if(front == NULL)
-        {
-            front = ptr;
-            rear = ptr;
-            front -> next = NULL;
-            rear -> next = NULL;
-        }
-        else
-        {
-            rear -> next = ptr;
-            rear = ptr;
-            rear->next = NULL;
-        }
-    }
+    
 }
 void del ()
 {
