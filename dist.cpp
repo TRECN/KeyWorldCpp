368d2809778980dbce673569ef94775109aa08c1
TRECN <rishabh1103@outlook.com>
2021-12-24 22:06:43 +0530

    commit

--- dist.cpp
+++ dist.cpp
@@ -3,7 +3,11 @@ using namespace std;
 class S
 {
     public:
-     
+        static int a;
+        S (){
+            a++;
+            cout<<"constructor : "<<a<<endl;
+        }
 
         ~S(){
             a++;
