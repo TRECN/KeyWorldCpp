6ff7bb4d1f33051814750874388672844468291c
TRECN <rishabh1103@outlook.com>
2021-12-24 22:15:18 +0530

    commit

--- llqueue.c
+++ llqueue.c
@@ -38,33 +38,6 @@ void main ()
    }
 }
 
-void ins(){
-    struct node *ptr;
-    int item;
-    ptr = (struct node *) malloc (sizeof(struct node));
-    if(ptr == NULL){
-        printf("\nOVERFLOW\n");
-        return;
-    }
-    else{
-        printf("Enter value:");
-        scanf("%d",&item);
-        ptr -> data = item;
-        if(front == NULL)
-        {
-            front = ptr;
-            rear = ptr;
-            front -> next = NULL;
-            rear -> next = NULL;
-        }
-        else
-        {
-            rear -> next = ptr;
-            rear = ptr;
-            rear->next = NULL;
-        }
-    }
-}
 void del ()
 {
     struct node *ptr;
