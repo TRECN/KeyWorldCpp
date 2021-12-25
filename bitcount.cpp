6c3ac997e4f863f141295ef9ae872f5723bcaa5e
TRECN <rishabh1103@outlook.com>
2021-12-25 22:27:45 +0530

    commit

--- bitcount.cpp
+++ bitcount.cpp
@@ -1,16 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int numSetBits( int A){
-    int count=0;
-    int d=A&1;
-    while((A&1)==0){
-        count++;
-        A=A>>1;
-        
-    }
-    return count;
-}
+
 
 int main(){
     cout<<numSetBits(8);
