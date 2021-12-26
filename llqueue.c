b66464f4da53ceb0299dd569320fb0727802dcc5
TRECN <rishabh1103@outlook.com>
2021-12-26 21:58:53 +0530

    commit

--- llqueue.c
+++ llqueue.c
@@ -51,12 +51,7 @@ void ins(){
         scanf("%d",&item);
         ptr -> data = item;
         if(front == NULL)
-        {
-            front = ptr;
-            rear = ptr;
-            front -> next = NULL;
-            rear -> next = NULL;
-        }
+        
         else
         {
             rear -> next = ptr;
