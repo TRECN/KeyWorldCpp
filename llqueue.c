85df9f2e0c4e80f259929722b8210f3e456121c2
TRECN <rishabh1103@outlook.com>
2021-12-26 21:59:28 +0530

    commit

--- llqueue.c
+++ llqueue.c
@@ -50,7 +50,19 @@ void ins(){
         printf("Enter value:");
         scanf("%d",&item);
         ptr -> data = item;
-        
+        if(front == NULL)
+        {
+            front = ptr;
+            rear = ptr;
+            front -> next = NULL;
+            rear -> next = NULL;
+        }
+        else
+        {
+            rear -> next = ptr;
+            rear = ptr;
+            rear->next = NULL;
+        }
     }
 }
 void del ()
