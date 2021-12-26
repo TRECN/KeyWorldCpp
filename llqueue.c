158b71a3bc1a0b05a39c5b25e1b667a8d404b352
TRECN <rishabh1103@outlook.com>
2021-12-26 22:01:06 +0530

    commit

--- llqueue.c
+++ llqueue.c
@@ -80,7 +80,13 @@ void del ()
 }
 void display()
 {
-    
+    struct node *ptr;
+    ptr = front;
+    if(front == NULL)
+    {
+        printf("\nEmpty queue");
+    }
+    else
     {   
         printf("Elements of Queue:");
         while(ptr != NULL)
