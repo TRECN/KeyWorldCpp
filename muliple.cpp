c0dc41c33b1e03abd0be868021aa1568e06464a2
TRECN <rishabh1103@outlook.com>
2021-12-27 22:30:12 +0530

    commit

--- muliple.cpp
+++ muliple.cpp
@@ -2,12 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class student{
-    protected:
-        void show(){
-            cout<<"student class inherited."<<endl;
-        }
-};
+
 
 class detail{
     protected:
