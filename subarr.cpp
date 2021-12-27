dd602aa47a6b307a7bbf542c49cd55a9cf4d9be7
TRECN <rishabh1103@outlook.com>
2021-12-27 22:31:46 +0530

    commit

--- subarr.cpp
+++ subarr.cpp
@@ -3,7 +3,12 @@ using namespace std;
 
 void subar(int a[],int n){
     for(int i=0;i<n;i++){
-       
+        for(int j=i;j<n;j++){
+            for(int k=i;k<=j;k++){
+                cout<<a[k]<<" ";
+            }
+            cout<<endl;
+        }
     }
 }
 
