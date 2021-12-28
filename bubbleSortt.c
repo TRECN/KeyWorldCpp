94b13ec64fc28aea98fdfae370b91f244ceb7d3f
TRECN <rishabh1103@outlook.com>
2021-12-28 22:33:08 +0530

    commit

--- bubbleSortt.c
+++ bubbleSortt.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<conio.h>
-void swap(int *xp, int *yp) 
-{ 
-    int temp = *xp; 
-    *xp = *yp; 
-    *yp = temp; 
-} 
+ 
 void bubbleSort(int arr[], int n) 
 { 
    int i, j; 
