b9d2da8212ea2303b77f1abf80cef5c2ef30bacb
TRECN <rishabh1103@outlook.com>
2021-12-28 22:32:17 +0530

    commit

--- bubbleSortt.c
+++ bubbleSortt.c
@@ -22,3 +22,12 @@ void printArray(int arr[], int size)
         printf("%d ", arr[i]); 
     printf("\n"); 
 } 
+int main() 
+{ 
+    int arr[] = {64, 34, 205, 112, 2, 1, 9}; 
+    int n = sizeof(arr)/sizeof(arr[0]); 
+    bubbleSort(arr, n); 
+    printf("Sorted array: \n"); 
+    printArray(arr, n); 
+    return 0; 
+}
