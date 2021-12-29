ab3af223c6ce3b3953960fcd57be3052c2365124
TRECN <rishabh1103@outlook.com>
2021-12-29 22:17:01 +0530

    commit

--- stackk.cpp
+++ stackk.cpp
@@ -13,13 +13,7 @@ void push(int *arr,int *top,int n){
 }
 
 void pop(int *arr,int *top,int n){
-    int data;
-    if(*top<0)
-        cout<<"underflow!!"<<endl;
-    else{
-        arr[*top]=NULL;
-        *top=*top-1;
-    }
+
 }
 
 
