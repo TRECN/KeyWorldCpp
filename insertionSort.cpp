4e6e9858dbcb703c6a342d10db102a16e59a1c68
TRECN <rishabh1103@outlook.com>
2021-12-30 23:29:25 +0530

    commit

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -7,15 +7,7 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-     for(int i=0;i<n;i++){
-         int c=a[i];
-         int j=i-1;
-         while(a[j]>c&&j>=0){
-             a[i]=a[j];
-             j--;
-         }
-         a[j+1]=c;
-     }
+    
       for(int i=0;i<n;i++){
         cout<<a[i]<<" ";
     }
