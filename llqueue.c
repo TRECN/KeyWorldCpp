4c77262ff6d05649389ebfba07f099df8de921e5
TRECN <rishabh1103@outlook.com>
2022-01-02 22:52:00 +0530

    commit

--- llqueue.c
+++ llqueue.c
@@ -42,7 +42,10 @@ void ins(){
     struct node *ptr;
     int item;
     ptr = (struct node *) malloc (sizeof(struct node));
-    
+    if(ptr == NULL){
+        printf("\nOVERFLOW\n");
+        return;
+    }
     else{
         printf("Enter value:");
         scanf("%d",&item);
