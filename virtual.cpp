59a284428ca7c985c547622fafc5539ea4563b40
TRECN <rishabh1103@outlook.com>
2022-01-25 22:04:55 +0530

    commit

--- virtual.cpp
+++ virtual.cpp
@@ -6,21 +6,3 @@ class A{
         int a;
 };
 
-class C:public virtual A{
-    public:
-        void display(){
-            cout<<a;
-        }
-};
-
-class D:public B,public C{
-    public:
-        B::display();
-        C::display();
-};
-
-
-int main(){
-    D obj;
-    obj.a=10;
-}
