41d1b7f0b27d0702900cba792df6d183bf20af5d
TRECN <rishabh1103@outlook.com>
2022-01-25 22:04:59 +0530

    comit

--- virtual.cpp
+++ virtual.cpp
@@ -6,3 +6,21 @@ class A{
         int a;
 };
 
+class C:public virtual A{
+    public:
+        void display(){
+            cout<<a;
+        }
+};
+
+class D:public B,public C{
+    public:
+        B::display();
+        C::display();
+};
+
+
+int main(){
+    D obj;
+    obj.a=10;
+}
