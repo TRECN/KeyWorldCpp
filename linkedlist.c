6b9844d11e1e49998e461f8c33ad19b3b90bf7e4
TRECN <rishabh1103@outlook.com>
2022-01-25 22:02:17 +0530

    commit

--- linkedlist.c
+++ linkedlist.c
@@ -50,7 +50,21 @@ void sorting(){
 
 }
 
+void inend(){
+    struct node *p,*q;
+    q=(struct node*)malloc(sizeof(struct node));
+    printf("enter data: ");
+    scanf("%d",&q->data);
+    p=start;
+    while(p->next!=NULL){
+        p=p->next;
+    }
+        q->next=NULL;
+        p->next=q;
 
+    printf("---------\n\n");
+        
+}
 
 void inbeg(){
     struct node* p;
