76fe92b15bac62fe8a6f01453ebace4088a0a407
TRECN <rishabh1103@outlook.com>
2022-01-26 22:09:55 +0530

    commit

--- selectionSort.cpp
+++ selectionSort.cpp
@@ -8,6 +8,12 @@ int main(){
         cin>>a[i];
     }
 
+    for(int i=0;i<n-1;i++){
+        int min=i;
+        for(int j=i+1;j<n;j++){
+            if(a[j]<a[min]){
+                min=j;
+            }
             
         }
         int t=a[i];
