4dd76e23511a8d0adf3bb7c114a22becde58c77f
TRECN <rishabh1103@outlook.com>
2022-01-26 22:12:13 +0530

    commit

--- clas.cpp
+++ clas.cpp
@@ -1,11 +1,6 @@
 #include<iostream>
 using namespace std;
-class a{
-    public:
-        int multi(int a,int b){
-            return a*b;
-        }
-};
+
 int main(){
     a obj;
     cout<<obj.multi(2,5)<<endl;
