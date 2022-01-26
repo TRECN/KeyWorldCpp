e48385b90efaf1c986e06cd45303a54d4ba5e4fc
TRECN <rishabh1103@outlook.com>
2022-01-26 22:07:59 +0530

    commit

--- ug.cpp
+++ ug.cpp
@@ -28,7 +28,15 @@ public:
 			cout << "x = " << -c << endl;
 
 		}
+		else if (discriminant > 0) {
 
+			x1 = (-b + sqrt(discriminant)) / (2 * a);
+			x2 = (-b - sqrt(discriminant)) / (2 * a);
+			cout << "Roots are real and different" << endl;
+			cout << "x1 = " << x1 << endl;
+			cout << "x2 = " << x2 << endl;
+
+		}
 		else if (discriminant == 0) {
 
 			cout << "Roots are real and equal" << endl;
