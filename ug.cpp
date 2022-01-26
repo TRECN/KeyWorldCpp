3a3655d6edc09231ffd822fe20581412bc26a7f4
TRECN <rishabh1103@outlook.com>
2022-01-26 22:07:22 +0530

    commit

--- ug.cpp
+++ ug.cpp
@@ -39,10 +39,6 @@ public:
 		}
 		else if (discriminant == 0) {
 
-			cout << "Roots are real and equal" << endl;
-			x1 = (-b + sqrt(discriminant)) / (2 * a);
-			cout << "x =" << x1 << endl;
-
 		}
 		else {
 
