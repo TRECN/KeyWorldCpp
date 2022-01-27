cfedbf37f80cc6a2145371ac82d229e9b992ee9b
TRECN <rishabh1103@outlook.com>
2022-01-27 21:07:54 +0530

    commit

--- assignment4.cpp
+++ assignment4.cpp
@@ -68,6 +68,15 @@ int main(){
     int rear=4;
     insertion(q,6,&front,&rear,'F');
     show(q,6,&front,&rear);
+    
+    deletion(q,6,&front,&rear);
+    deletion(q,6,&front,&rear);
+    show(q,6,&front,&rear);
+
+    insertion(q,6,&front,&rear,'K');
+    insertion(q,6,&front,&rear,'L');
+    insertion(q,6,&front,&rear,'M');
+    show(q,6,&front,&rear);
 
     deletion(q,6,&front,&rear);
     deletion(q,6,&front,&rear);
