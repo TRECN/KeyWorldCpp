518aa395543c6148645f14b3a26ecc53803c45fc
TRECN <rishabh1103@outlook.com>
2022-01-27 21:03:48 +0530

    commit

--- assignment4.cpp
+++ assignment4.cpp
@@ -1,7 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+void insertion(char *q,int n,int *front,int *rear,char data){
+    if((*front==0&&*rear==n-1)||(*front==*rear+1)){
+        cout<<"queue is overflow";
+    }
+    else{
+        if(*front==-1){
+            *front=0;
+            *rear=0;
+        }
+        else if(*rear==n-1)
+            *rear=0;
+        else
+            *rear=*rear+1;
 
+        q[*rear]=data;
+
+    }
+}
 
 
 
