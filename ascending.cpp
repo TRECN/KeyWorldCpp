3aabaff81598e1ad8d58f22133ebf901dd355880
TRECN <rishabh1103@outlook.com>
2022-01-28 23:30:43 +0530

    commit

--- ascending.cpp
+++ ascending.cpp
@@ -1,4 +1,13 @@
-
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    
     for(int i=0; i<n; i++)
     {
         for(int j=i+1; j<n; j++)
