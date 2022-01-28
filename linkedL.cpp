3c00ab7e33b16c4b293f698ed0c366a486382c39
TRECN <rishabh1103@outlook.com>
2022-01-28 23:24:30 +0530

    commit

--- linkedL.cpp
+++ linkedL.cpp
@@ -13,7 +13,18 @@ class node{
 
 };
 
-
+void insetAtTail(node* &head,int val){
+    node* n=new node(val);
+    if(head==NULL){
+        head=n;
+        return;
+    }
+    node* temp = head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
 
 void display(node* head){
     node* temp=head;
