a2b0f8709a0c1c798564d57634a4b99f207b589b
TRECN <rishabh1103@outlook.com>
2022-01-28 23:29:52 +0530

    commit

--- stackk.cpp
+++ stackk.cpp
@@ -30,6 +30,17 @@ void show(int *arr,int *top,int n){
     cout<<endl;
 }
 
+int main(){
+    int n;
+    cout<<"enter the size of stack: "<<endl;
+    cin>>n;
+    int arr[n];
+    int top=-1;
+    int ch=1;
+    while(ch>0){
+        cout<<"enter choice: 1. push, 2. pop, 3. show, 0 to quit"<<endl;
+        cin>>ch;
+        switch (ch)
         {
         case 0:
             break;
