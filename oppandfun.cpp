e1acbfca5c9fa26d64208a2e3c3a1ace7520d526
TRECN <rishabh1103@outlook.com>
2022-01-29 22:10:05 +0530

    commit

--- oppandfun.cpp
+++ oppandfun.cpp
@@ -2,14 +2,6 @@
 using namespace std;
 class A{
     
-    public:
-        int a;
-        int b;
-        A(){}
-        A(int a1,int b1){
-            a=a1;
-            b=b1;
-        }
 
         friend A operator+= (A j,A i){
             A k;
