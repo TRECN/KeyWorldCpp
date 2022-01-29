6a377a73e49fe4fea1c5b00f78bba6a10b440c45
TRECN <rishabh1103@outlook.com>
2022-01-29 22:07:12 +0530

    commit

--- stackk.cpp
+++ stackk.cpp
@@ -23,6 +23,13 @@ void pop(int *arr,int *top,int n){
 }
 
 
+void show(int *arr,int *top,int n){
+    for(int i=0;i<=*top;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
 int main(){
     int n;
     cout<<"enter the size of stack: "<<endl;
