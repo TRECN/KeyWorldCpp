b97904304c9aa5ef5d8bcac01c46d6d22bc2672f
TRECN <rishabh1103@outlook.com>
2022-01-31 21:06:53 +0530

    commit

--- queFirst.c
+++ queFirst.c
@@ -3,7 +3,16 @@
 #include<stdlib.h>  
 void front(int);  
 
-
+struct node *head;  
+void display()  
+{  
+    struct node *ptr;  
+    ptr = head;   
+    if(ptr == NULL)  
+    {  
+        printf("Nothing to print");  
+    }  
+    else  
     {  
         printf("\nprinting values . . . . .\n");   
         while (ptr!=NULL)  
