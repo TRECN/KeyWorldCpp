7433ea746cfcac27f497fbd79ae7dbd9919c0402
TRECN <rishabh1103@outlook.com>
2022-01-31 21:08:20 +0530

    commit

--- binary.cpp
+++ binary.cpp
@@ -3,7 +3,11 @@ using namespace std;
 int main(){
     int n;
     bool c=false;
-
+    cout<<"enter the length of array: ";
+    cin>>n;
+    int begin=0,end=n-1,el,mid,i;
+    
+    int a[n];
     cout<<"enter the elements: "<<endl;
     for(i=0;i<n;i++)
         cin>>a[i];
