949b26a63a6826cd8012da065f063515f87924ae
TRECN <rishabh1103@outlook.com>
2022-01-31 21:07:52 +0530

    commit

--- queFirst.c
+++ queFirst.c
@@ -22,3 +22,19 @@ void display()
         }  
     }  
 } 
+
+void front(int item)  
+    {  
+        struct node *ptr = (struct node *)malloc(sizeof(struct node *));  
+        if(ptr == NULL)  
+        {  
+            printf("\nOVERFLOW !!!\n");  
+        }  
+        else  
+        {  
+            ptr->data = item;  
+            ptr->next = head;  
+            head = ptr;  
+            printf("\nGiven node is inserted\n");  
+        }            
+    } 
