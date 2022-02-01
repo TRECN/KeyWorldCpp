695e17e7fe03e3241cbc28d5d22fb0d829cf8538
TRECN <rishabh1103@outlook.com>
2022-02-01 22:43:10 +0530

    commit

--- oddE.c
+++ oddE.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+int main()
+{
+    int arr[20], size, i, temp;
+    printf("Enter the array of size\n");
+    scanf("%d", &size);
+    printf("Enter the element of array\n");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < size; i= i + 2)
+    {
+        temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+    }
+    printf("Interchange array is:");
+     for (int i = 0; i < size; i++)
+     {
+        printf(" %d\n", arr[i]);
+     }
+    return 0;
+}
 
