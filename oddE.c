6b3e115e226917ad2a188515a91179d0ea45bbab
TRECN <rishabh1103@outlook.com>
2022-02-01 22:36:53 +0530

    commit

--- oddE.c
+++ oddE.c
@@ -1,4 +1,10 @@
-printf("Enter the element of array\n");
+#include <stdio.h>
+int main()
+{
+    int arr[20], size, i, temp;
+    printf("Enter the array of size\n");
+    scanf("%d", &size);
+    printf("Enter the element of array\n");
     for (int i = 0; i < size; i++)
     {
         scanf("%d", &arr[i]);
