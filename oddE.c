8ef6fb4f09b3b2cf2b74afcd45be4ee90985d287
TRECN <rishabh1103@outlook.com>
2022-02-01 22:40:02 +0530

    commit

--- oddE.c
+++ oddE.c
@@ -15,11 +15,5 @@ int main()
         arr[i] = arr[i + 1];
         arr[i + 1] = temp;
     }
-    printf("Interchange array is:");
-     for (int i = 0; i < size; i++)
-     {
-        printf(" %d\n", arr[i]);
-     }
-    return 0;
-}
+
 
