dfae16677a533e2eba09ceddc08c30fd1e98b370
TRECN <rishabh1103@outlook.com>
2022-02-02 21:05:01 +0530

    commit

--- hierar.cpp
+++ hierar.cpp
@@ -24,7 +24,15 @@ class car2:public cars{
         }
 
 };
+class car3:public cars{
+    public:
+        car3(int no){
+            cout<<no<<" ";
+            cout<<company<<endl;
+            
+        }
 
+};
 
 
 int main(){
