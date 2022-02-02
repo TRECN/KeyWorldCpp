4347c66e4e88852b448739339c46ae460011556e
TRECN <rishabh1103@outlook.com>
2022-02-02 21:09:54 +0530

    commit

--- maxMinArr.c
+++ maxMinArr.c
@@ -1,14 +1,4 @@
-#include<stdio.h>
-int main()
-{
-    int arr[20], i, size, max, min,c,d;
-    printf("Enter the array of size\n");
-    scanf("%d", &size);
-    printf("Enter the element of Array\n");
-    for (int i = 0; i < size; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
+
     max = arr[0];
     for (int i = 0; i < size; i++)
     {
