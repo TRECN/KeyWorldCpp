3dc2ce6127f3a1375700725ad5add09ba1be7552
TRECN <rishabh1103@outlook.com>
2022-02-02 21:08:52 +0530

    commit

--- maxMinArr.c
+++ maxMinArr.c
@@ -5,27 +5,7 @@ int main()
     printf("Enter the array of size\n");
     scanf("%d", &size);
     printf("Enter the element of Array\n");
-    for (int i = 0; i < size; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    max = arr[0];
-    for (int i = 0; i < size; i++)
-    {
-        if (max<arr[i])
-        {
-            max = arr[i];
-            c = i+1;
-        }
-    }
-    min = arr[0];
-    for (int i = 0; i < size; i++)
-    {
-        if (min>arr[i])
-        {
-            min = arr[i];
-            d = i+1;
-        }
+
     }
     printf("The max value is '%d' at the position of %d\n",max,c);
     printf("The min value is '%d' at the position of %d\n",min,d);
