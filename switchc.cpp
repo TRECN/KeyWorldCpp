250c66fae6e0e75275019462362f778ffee2a8e6
TRECN <rishabh1103@outlook.com>
2022-02-02 21:06:05 +0530

    commit

---
+++ switchc.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"enter a no. between 1-7 to show week days."<<endl;
+    int n;
+    cin>>n;
+    switch(n){
+        case 1:
+            cout<<"monday";
+        break;
+        case 2:
+            cout<<"tuesday";
+        break;
+        case 3:
+            cout<<"wednesday";
+        break;
+        case 4:
+            cout<<"thursday";
+        break;
+        
+    }
+    return 0;
+}
