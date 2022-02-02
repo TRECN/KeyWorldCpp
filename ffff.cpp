41009734e6f82a94adfa5c4a3c3f661de06eb2b7
TRECN <rishabh1103@outlook.com>
2022-02-02 21:07:19 +0530

    commit

--- ffff.cpp
+++ ffff.cpp
@@ -22,3 +22,14 @@ int main()
 	}
 	  
 	
+	cout<<"after concatenation:"<<endl;
+	for(i=0;i<=s;i++)
+	{
+       cout<<"a[i]=s"<<endl;
+    }
+    for( j=0;j<=t;j++)
+    {
+    	cout<<"a[j]=s"<<endl;
+    }
+    return 0;
+}
