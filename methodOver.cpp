e347b9fb12891b81d6039be50e0a57e4eca824ed
TRECN <rishabh1103@outlook.com>
2022-02-02 21:06:33 +0530

    commit

--- methodOver.cpp
+++ methodOver.cpp
@@ -1,6 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+class sum{
+    public:
+        void show(){
+            int a=10,b=20;
+            cout<<a+b<<endl;
+        }
+};
 
 class diff:public sum{
     public:
