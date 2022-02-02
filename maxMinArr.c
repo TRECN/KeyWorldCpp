2360dbf8b479ae9cef1ed386f2b9394e9a69e501
TRECN <rishabh1103@outlook.com>
2022-02-02 21:08:46 +0530

    commit

--- maxMinArr.c
+++ maxMinArr.c
@@ -18,3 +18,16 @@ int main()
             c = i+1;
         }
     }
+    min = arr[0];
+    for (int i = 0; i < size; i++)
+    {
+        if (min>arr[i])
+        {
+            min = arr[i];
+            d = i+1;
+        }
+    }
+    printf("The max value is '%d' at the position of %d\n",max,c);
+    printf("The min value is '%d' at the position of %d\n",min,d);
+    return 0; 
+}
