82babe95f716fea91c7988f41e50434ec0008cd1
TRECN <rishabh1103@outlook.com>
2022-02-03 21:37:33 +0530

    commit

--- sumArr.cpp
+++ sumArr.cpp
@@ -1,7 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 class Solution{
-
+    public:
+    int getSum(int a[],int n){
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=a[i];
+        }
         return sum;
     }
 };
