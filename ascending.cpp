d4ec563fbc0a7c4dbf790213881e19dd73009997
TRECN <rishabh1103@outlook.com>
2022-02-03 21:32:50 +0530

    commit

--- ascending.cpp
+++ ascending.cpp
@@ -7,23 +7,3 @@ int main(){
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
-    
-    for(int i=0; i<n; i++)
-    {
-        for(int j=i+1; j<n; j++)
-        {
-            
-            if(a[j] < a[i])
-            {
-                int temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
-            }
-        }
-    }
-    cout<<"Elements of array in sorted ascending order:"<<endl;
-    for(int i=0; i<n; i++)
-    {
-        cout<<a[i]<<endl;
-    }
-}
