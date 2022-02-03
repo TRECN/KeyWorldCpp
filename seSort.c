86b3ad46300562e18b25dd9a588eaf716454642e
TRECN <rishabh1103@outlook.com>
2022-02-03 21:38:41 +0530

    commit

--- seSort.c
+++ seSort.c
@@ -1,4 +1,14 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include<conio.h>
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
 void selectionSort(int arr[], int n)
 {
     int i, j, min_idx;
