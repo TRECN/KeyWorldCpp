8943775dfff2ee8c0bc9e2a261e37a13bbd2884e
TRECN <rishabh1103@outlook.com>
2022-02-03 21:35:37 +0530

    commit

--- qualprel.cpp
+++ qualprel.cpp
@@ -1,16 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int t,n,k;
-    cin>>t;
-    while(t-->0){
-        cin>>n>>k;
-        int c=0;
-        k--;
-        int a[n];
-        for(int i=0;i<n;i++){
-            cin>>a[i];
-        }
+
         int f = sizeof(a) / sizeof(a[0]);
         sort(a, a + f, greater<int>());
         for(int i=0;i<n;i++){
