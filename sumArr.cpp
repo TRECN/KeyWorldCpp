a359fa36338a80e44e04dff659c1f2949e58909a
TRECN <rishabh1103@outlook.com>
2022-02-03 21:38:06 +0530

    commit

--- sumArr.cpp
+++ sumArr.cpp
@@ -11,6 +11,15 @@ class Solution{
     }
 };
 int main(){
-
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int a[n];
+        fir(int i=0;i<n;i++) cin>>a[i];
+        Solution ob;
+        cout<<ob.getSum(a,n)<<endl;
+    }
     return 0;
 }
