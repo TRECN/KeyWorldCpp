beb29b8fa29a4cf9838b3ed113ab9d2e8e1ab293
TRECN <rishabh1103@outlook.com>
2022-02-03 21:36:28 +0530

    commit

--- qualprel.cpp
+++ qualprel.cpp
@@ -8,3 +8,17 @@ int main(){
         int c=0;
         k--;
         int a[n];
+        for(int i=0;i<n;i++){
+            cin>>a[i];
+        }
+        int f = sizeof(a) / sizeof(a[0]);
+        sort(a, a + f, greater<int>());
+        for(int i=0;i<n;i++){
+            if(a[i]>=a[k])
+                c++;
+        }
+        cout<<c<<endl;
+        
+    }
+    return 0;
+}
