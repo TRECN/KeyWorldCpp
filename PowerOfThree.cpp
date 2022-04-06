7dc40b267833d08d7bddb23c75f8153b908a1e8e
TRECN <rishabh1103@outlook.com>
2022-04-06 22:37:37 +0530

    commit

--- PowerOfThree.cpp
+++ PowerOfThree.cpp
@@ -4,11 +4,10 @@ public:
         if(n<=0)
             return false;
         while(n%3==0){
-            if(n==1)
-                return true;
             n/=3;
         }
-
+        if(n==1)
+            return true;
         return false;
     }
 };
