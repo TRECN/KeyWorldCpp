07516e5cfe925628f7c923c36d6bd3a5cae83d4f
TRECN <rishabh1103@outlook.com>
2022-05-12 16:59:46 +0530

    commiyt

---
+++ maximumSubArr.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int (int a[],int n){
+    
+}
+int main(){
+    int a[]={-8,-2,6,-5,10,3,-3,10};
+    int n=sizeof(a)/sizeof(a[0]);
+    cout<<kadane(a,n);
+    return 0;
+}
