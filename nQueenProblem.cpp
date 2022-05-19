ba772e224351298e781c43b9906c3779ea819d12
TRECN <rishabh1103@outlook.com>
2022-05-19 17:38:19 +0530

    commit

--- nQueenProblem.cpp
+++ nQueenProblem.cpp
@@ -54,12 +54,12 @@ int main(){
     int n;
     cin>>n;
     int** arr=new int*[n];
-    for(int i=0;i<n;i++){
+    `for(int i=0;i<n;i++){
         arr[i]=new int[n];
         for(int j=0;j<n;j++){
             arr[i][j]=0;
         }
-    }
+    }`
 
     if(nQueen(arr,0,n)){
         for(int i=0;i<n;i++){
