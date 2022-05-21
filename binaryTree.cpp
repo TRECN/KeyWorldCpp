ad80917b533813da543ddb816a0641e0d048bbe6
TRECN <rishabh1103@outlook.com>
2022-05-21 21:36:26 +0530

    commit

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -12,7 +12,29 @@ class Node{
             right=NULL;
         }
 };
+void Preorder(Node* node){
+    if(node==NULL)
+        return;
+    cout<<node->data<<" ";
+    Preorder(node->left);
+    Preorder(node->right);
+}
 
+void Postorder(Node* node){
+    if(node==NULL)
+        return;
+    Postorder(node->left);
+    Postorder(node->right);
+    cout<<node->data<<" ";
+}
+
+void Inorder(Node* node){
+    if(node==NULL)
+        return;
+    Inorder(node->left);
+    cout<<node->data<<" ";
+    Inorder(node->right);
+}
 int main(){
     Node* root=new Node(1);
 
@@ -20,4 +42,15 @@ int main(){
     root->right=new Node(3);
     
     root->left->left=new Node(4);
+    root->left->right=new Node(5);
+
+    cout << "\nPreorder traversal of binary tree is \n";
+    Preorder(root);
+ 
+    cout << "\nInorder traversal of binary tree is \n";
+    Inorder(root);
+ 
+    cout << "\nPostorder traversal of binary tree is \n";
+    Postorder(root);
+
 }
