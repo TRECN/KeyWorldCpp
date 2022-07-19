b2ad7e7be32683b036f14ce5f842c95607052738
TRECN <rishabh1103@outlook.com>
2022-07-19 18:06:20 +0530

    commit

--- subsecRec.cpp
+++ subsecRec.cpp
@@ -7,3 +7,19 @@ void printF(vector<int>a){
     }
     cout<<endl;
 }
+void sec(int i,int *arr,vector<int>a,int n){
+    if(i>=n){
+        printF(a);
+        return;
+    }
+    a.push_back(arr[i]);
+    sec(i+1,arr,a,n);
+    a.pop_back();
+    sec(i+1,arr,a,n);
+}
+int main(){
+    int arr[]={3,1,2};
+    vector<int>a;
+    int n=3;
+    sec(0,arr,a,n);
+}
